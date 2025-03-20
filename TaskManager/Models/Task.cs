75fcba79608b461308d86db9f36e4c330d6f1aab
chris <christopher002840@gmail.com>
2025-03-20 16:53:03 +0800

    加入專案檔案。

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
